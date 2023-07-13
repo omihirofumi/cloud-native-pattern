a3027f8073201d3fd76a1a6d403e5d45e82263dd
hirofumi omi <omihirofumi@gmail.com>
2023-07-13 22:41:51 +0900

    update throttle v2 impl load shedding

--- throttlev2/main.go
+++ throttlev2/main.go
@@ -6,8 +6,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync/atomic"
 	"time"
 )
+
+const MAX_QUEUE = 10
 
 var throttled = Throttle(getHostname, 1, 1, time.Second)
 
@@ -34,7 +37,19 @@ func throttledHandler(c echo.Context) error {
 }
 
 func main() {
+	var queueCnt int64
 	e := echo.New()
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			atomic.AddInt64(&queueCnt, 1)
+			defer atomic.AddInt64(&queueCnt, -1)
+
+			if atomic.LoadInt64(&queueCnt) > MAX_QUEUE {
+				return echo.NewHTTPError(http.StatusTooManyRequests, "server is busy.")
+			}
+			return next(c)
+		}
+	})
 	e.GET("/hostname", throttledHandler)
 	log.Fatal(e.Start(":8080"))
 }
